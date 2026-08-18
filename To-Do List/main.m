0ad5e28a94ad290583a1efb3846b45e1bdedb1d6
Tyler Pierce <ecreipt@gmail.com>
2026-08-18 09:55:54 -0600

    Initial Commit

---
+++ To-Do List/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  To-Do List
+//
+//  Created by Tyler Pierce on 8/18/26.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return EXIT_SUCCESS;
+}
